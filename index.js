71121b192fc4d45b3a0aaf9c2f695fe3ab237e16
Juan Pablo Cuervo Dorado <104020840+juancuerv@users.noreply.github.com>
2022-12-11 22:50:33 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -48,9 +48,10 @@ ClienteARI.connect('http://localhost:8088', 'asterisk', 'asterisk', function (er
       switch (digit) {
         case '1':    //Consultar resultados de apuestas
           incoming.removeListener('ChannelDtmfReceived', introMenu);
+          //play(channel, 'sound:ConsultaApuestaCedula');
           console.log('- ConsultarApuesta -');
-	        text='Por favor digite su cédula, seguida de la tecla numeral.';
-	        await generarAudio(text);
+	 text='Por favor digite su cédula, seguida de la tecla numeral.';
+	  await generarAudio(text);
           await convertirAudio();
           await play(incoming,pathAudios);
           console.log('Digite su cedula seguido de la tecla #');
@@ -59,12 +60,13 @@ ClienteARI.connect('http://localhost:8088', 'asterisk', 'asterisk', function (er
           break;
 
         case '2': //Realizar una nueva apuesta
-          incoming.removeListener('ChannelDtmfReceived', introMenu);          
+          incoming.removeListener('ChannelDtmfReceived', introMenu);
+          //play(channel, 'sound:nuevaApuesta');
           console.log('- RealizarApuesta -');
-	        text='Por favor digite su cédula, seguida de la tecla numeral.';
-	        await generarAudio(text);
- 	        await convertirAudio();
- 	        await play(incoming,pathAudios);
+	  text='Por favor digite su cédula, seguida de la tecla numeral.';
+	  await generarAudio(text);
+ 	  await convertirAudio();
+ 	  await play(incoming,pathAudios);
           console.log('Digite su cedula seguido de la tecla #');
           console.log('Si está inconforme con el número ingresado, digite *');
           nuevaA(event, incoming);
@@ -107,6 +109,7 @@ ClienteARI.connect('http://localhost:8088', 'asterisk', 'asterisk', function (er
     var playback = ari.Playback();
     playback.once('PlaybackFinished',
       function (event, instance) {
+
         if (callback) {
           callback(null);
         }
@@ -118,17 +121,18 @@ ClienteARI.connect('http://localhost:8088', 'asterisk', 'asterisk', function (er
 
   async function consultarApuesta(event, incoming) {
     let dato = event.digit;
+    // Grabacion de peticion de cedula y marcacion de #
     switch (dato) {
       case '#':
         incoming.removeListener('ChannelDtmfReceived', consultarApuesta);
-	      console.log('## Seleccion de tipo de apuesta a consultar ##');
+	console.log('## Seleccion de tipo de apuesta a consultar ##');
         console.log('1. última apuesta ganada y no pagada');
         console.log('2. Última apuesta pérdida');
         console.log('3. Última apuesta no jugada');
-	      text='Seleccion de tipo de apuesta a consultar: 1. última apuesta ganada y no pagada. 2. Última apuesta pérdida. 3. Última apuesta no jugada.';
-	      await generarAudio(text);
-	      await convertirAudio();
-	      await play(incoming, pathAudios);
+	text='Seleccion de tipo de apuesta a consultar: 1. última apuesta ganada y no pagada. 2. Última apuesta pérdida. 3. Última apuesta no jugada.';
+	await generarAudio(text);
+	await convertirAudio();
+	await play(incoming, pathAudios);
         incoming.on('ChannelDtmfReceived', consultaTipoA);
         break;
 
@@ -160,20 +164,20 @@ ClienteARI.connect('http://localhost:8088', 'asterisk', 'asterisk', function (er
             if (!resultado) return
             if(resultado.length!=0){
               resultaux=JSON.parse(JSON.stringify(resultado));
-              text ='Usted ha ganado la apuesta '+resultaux[0].local+' versus '+resultaux[0].visitante+' con el marcador '+resultaux[0].goles_local+' a '+resultaux[0].goles_visitante+' y el monto de '+resultaux[0].monto+'. Este partido se jugó el '+resultaux[0].fecha_partido.slice(0,10)+'.';
-	          }else{
+              text ='Usted ha ganado la apuesta '+resultaux[0].local+' versus '+resultaux[0].visitante+' con el marcador '+resultaux[0].goles_local+' a '+resultaux[0].goles_visitante+' y el monto de '+resultaux[0].monto+'. Este partido se jugó el '+resultaux[0].fecha_partido.slice(0,10)+'. ';
+	    }else{
               text = 'Usted no cuenta con apuestas ganadas. ';
               console.log('Resultado vacío');
             }
           })
-          .catch(text = 'La consulta realizada ha sido fallida, intente de nuevo.')
-	      query = '';
-	      text=text+'Gracias por preferirnos, esperamos que disfrutes de este mundial. Hasta pronto.';
+          .catch(text = 'La consulta realizada ha sido fallida, intente de nuevo. ')
+	  query = '';
+	text=text+'Gracias por preferirnos, esperamos que disfrutes de este mundial. Hasta pronto. ';
         console.log(text);
         await generarAudio(text);
         await convertirAudio();
         await play(incoming, pathAudios);
-	      setTimeout(function () {
+	setTimeout(function () {
           colgarLLamada(incoming);
         }, 20000)
         break;
@@ -190,21 +194,21 @@ ClienteARI.connect('http://localhost:8088', 'asterisk', 'asterisk', function (er
               console.log(resultado);
              if(resultado.length!=0){
                 resultaux=JSON.parse(JSON.stringify(resultado));
-                text ='Usted ha pérdido la apuesta '+resultaux[0].local+' versus '+resultaux[0].visitante+' con el marcador '+resultaux[0].goles_local+' a '+resultaux[0].goles_visitante+' y el monto de '+resultaux[0].monto+'. Este partido se jugò el '+resultaux[0].fecha_partido.slice(0,10)+'.';
+                text ='Usted ha pérdido la apuesta '+resultaux[0].local+' versus '+resultaux[0].visitante+' con el marcador '+resultaux[0].goles_local+' a '+resultaux[0].goles_visitante+' y el monto de '+resultaux[0].monto+'. Este partido se jugò el '+resultaux[0].fecha_partido.slice(0,10)+'. ';
               }else{
-                text = 'Usted no cuenta con apuestas pérdidas.';
+                text = 'Usted no cuenta con apuestas pérdidas. ';
                 console.log('Resultado vacío');
               }
             })
-            .catch(text = 'La consulta realizada ha sido fallida, intente de nuevo.')
+            .catch(text = 'La consulta realizada ha sido fallida, intente de nuevo. ')
 
         query = '';
-	      text=text+'Gracias por preferirnos, esperamos que disfrutes de este mundial. Hasta pronto.';
+	text=text+'Gracias por preferirnos, esperamos que disfrutes de este mundial. Hasta pronto. ';
         console.log(text);
         await generarAudio(text);
         await convertirAudio();
         await play(incoming, pathAudios);
-	      setTimeout(function () {
+	setTimeout(function () {
             colgarLLamada(incoming);
         }, 20000)
         break;
@@ -218,18 +222,18 @@ ClienteARI.connect('http://localhost:8088', 'asterisk', 'asterisk', function (er
         resultado = await consultadb(query)
             .then(function (resultado) {
               if(resultado.length!=0){
-		          resultaux=JSON.parse(JSON.stringify(resultado));
-                text ='Usted ha apostado al partido '+resultaux[0].local+' versus '+resultaux[0].visitante+' con el marcador '+resultaux[0].goles_local+' a '+resultaux[0].goles_visitante+' y el monto de '+resultaux[0].monto+'. Este partido se jugará el '+resultaux[0].fecha_partido.slice(0,10)+'.';
+		resultaux=JSON.parse(JSON.stringify(resultado));
+                text ='Usted ha apostado al partido '+resultaux[0].local+' versus '+resultaux[0].visitante+' con el marcador '+resultaux[0].goles_local+' a '+resultaux[0].goles_visitante+' y el monto de '+resultaux[0].monto+'. Este partido se jugará el '+resultaux[0].fecha_partido.slice(0,10)+'. ';
               }else{
                 text = 'Usted no ha realizado ninguna apuesta o todas ya cuantan con un resultado.';
                 console.log('Resultado vacío');
               }
             })
             .catch(text = 'La consulta realizada ha sido fallida, intente de nuevo')
-	      query = '';
-        text=text+'Gracias por preferirnos, esperamos que disfrutes de este mundial. Hasta pronto.';
-	      console.log(text);
-	      await generarAudio(text);
+	query = '';
+        text=text+'Gracias por preferirnos, esperamos que disfrutes de este mundial. Hasta pronto. ';
+	console.log(text);
+	await generarAudio(text);
         await convertirAudio();
         await play(incoming, pathAudios);
         setTimeout(function () {
@@ -240,9 +244,9 @@ ClienteARI.connect('http://localhost:8088', 'asterisk', 'asterisk', function (er
       default:
         incoming.removeListener('ChannelDtmfReceived', consultaTipoA);
         console.log('default');
-        text = 'Tipo de apuesta no válida, inténtelo de nuevo.';
+        text = 'Tipo de apuesta no válida, inténtelo de nuevo. ';
         console.log(text);
-	      await generarAudio(text);
+	await generarAudio(text);
         await convertirAudio();
         await play(incoming, pathAudios);
         incoming.on('ChannelDtmfReceived', consultarApuesta);
@@ -255,7 +259,7 @@ ClienteARI.connect('http://localhost:8088', 'asterisk', 'asterisk', function (er
     switch (dato2) {
       case '#':
         incoming.removeListener('ChannelDtmfReceived', realizarApuesta);
-	      console.log('Concluye ingreso de cédula');
+	console.log('Concluye ingreso de cèdula');
         incoming.on('ChannelDtmfReceived', elegirPartido);
         break;
 
@@ -282,23 +286,23 @@ ClienteARI.connect('http://localhost:8088', 'asterisk', 'asterisk', function (er
           if (!resultado) return
           if(resultado.length!=0){
             resultaux=JSON.parse(JSON.stringify(resultado));
-            text ='1. '+resultaux[0].local+' versus '+resultaux[0].visitante+'. Fecha: '+resultaux[0].fecha_partido.slice(0,10)+'.';
-            text =text+'2. '+resultaux[1].local+' versus '+resultaux[1].visitante+'. Fecha: '+resultaux[1].fecha_partido.slice(0,10)+'.';
+            text ='1. '+resultaux[0].local+' versus '+resultaux[0].visitante+'. Fecha: '+resultaux[0].fecha_partido.slice(0,10)+'. ';
+            text =text+'2. '+resultaux[1].local+' versus '+resultaux[1].visitante+'. Fecha: '+resultaux[1].fecha_partido.slice(0,10)+'. ';
             console.log(text);
-   	        await generarAudio(text);
+   	    await generarAudio(text);
             await convertirAudio();
             await play(incoming, pathAudios);
-   	        incoming.removeListener('ChannelDtmfReceived', elegirPartido);
+   	    incoming.removeListener('ChannelDtmfReceived', elegirPartido);
             incoming.on('ChannelDtmfReceived', elegirPartido2);
           }else{
-            text = 'No hay partido próximos para apostar.';
+            text = 'No hay partido próximos para apostar. ';
             console.log('Resultado vacío');
             console.log(text);
-   	        await generarAudio(text);
+   	    await generarAudio(text);
             await convertirAudio();
             await play(incoming, pathAudios);
             incoming.removeListener('ChannelDtmfReceived', elegirPartido);
-   	        setTimeout(function () {
+   	    setTimeout(function () {
               colgarLLamada(incoming);
             }, 4000)
           }
@@ -314,12 +318,12 @@ ClienteARI.connect('http://localhost:8088', 'asterisk', 'asterisk', function (er
         id_partido=resultaux[0].id_partido;
         incoming.removeListener('ChannelDtmfReceived', elegirPartido2);
         console.log('-- apuesta --');
-        text='ingrese los datos en el siguiente formato: marcador1, marcador2, monto de la apuesta y #. Sin espacios. Recuerde que el marcador de cada equipo sólo puede ir de 0 a 9.';
-	      console.log(text);
-	      await generarAudio(text);
+        text='ingrese los datos en el siguiente formato: marcador1, marcador2, monto de la apuesta y #. Sin espacios. Recuerde que el marcador de cada equipo sólo puede ir de 0 a 9. ';
+	console.log(text);
+	await generarAudio(text);
         await convertirAudio();
         await play(incoming, pathAudios);
-	      incoming.on('ChannelDtmfReceived', elegirPartido21);
+	incoming.on('ChannelDtmfReceived', elegirPartido21);
         break;
 
       case '2':
@@ -327,7 +331,7 @@ ClienteARI.connect('http://localhost:8088', 'asterisk', 'asterisk', function (er
         id_partido=resultaux[1].id_partido;
         incoming.removeListener('ChannelDtmfReceived', elegirPartido2);
         console.log('-- apuesta --');
-        text='ingrese los datos en el siguiente formato: marcador1, marcador2, monto de la apuesta y #. Sin espacios. Recuerde que el marcador de cada equipo sólo puede ir de 0 a 9.';
+        text='ingrese los datos en el siguiente formato: marcador1, marcador2, monto de la apuesta y #. Sin espacios. Recuerde que el marcador de cada equipo sólo puede ir de 0 a 9. ';
         console.log(text);
         await generarAudio(text);
         await convertirAudio();
@@ -337,7 +341,7 @@ ClienteARI.connect('http://localhost:8088', 'asterisk', 'asterisk', function (er
 
       default:
         console.log('default');
-        text = 'Opción no válida, inténtelo de nuevo';
+        text = 'Opción no válida, inténtelo de nuevo. ';
         console.log(text);
         await generarAudio(text);
         await convertirAudio();
@@ -354,7 +358,7 @@ ClienteARI.connect('http://localhost:8088', 'asterisk', 'asterisk', function (er
             incoming.removeListener('ChannelDtmfReceived', elegirPartido21);
             marcador1=datosapuesta.slice(0,1);
             marcador2=datosapuesta.slice(1,2);
-            monto=datosapuesta.slice(2,datosapuesta.length-1);
+            monto=datosapuesta.slice(2,datosapuesta.length);
             incoming.on('ChannelDtmfReceived', registrarApuesta);
             break;
 
@@ -384,7 +388,7 @@ ClienteARI.connect('http://localhost:8088', 'asterisk', 'asterisk', function (er
         break;
 
       case '*':
-        datosapuesta = datosapuesta.substring(0,datosapuesta.length);
+        datosapuesta = datosapuesta.substring(0,datosapuesta.length-1);
         console.log('retirando datos apuesta');
         console.log(datosapuesta);
         break;
@@ -409,23 +413,23 @@ ClienteARI.connect('http://localhost:8088', 'asterisk', 'asterisk', function (er
           id_usuario=resultaux[0].id_usuario;
           console.log(id_usuario);
         }else{
-          text = 'Usted no se encuentra registrado en nuestro sistema.';
+          text = 'Usted no se encuentra registrado en nuestro sistema. ';
           console.log('Resultado vacío');
- 	        await generarAudio(text);
+ 	  await generarAudio(text);
           await convertirAudio();
           await play(incoming, pathAudios);
           console.log(text);
           setTimeout(function () {
             colgarLLamada(incoming);
-          }, 6000)
+          }, 4000)
         }
       })
-      .catch(console.log('La consulta realizada ha sido fallida, intente de nuevo'))
-    console.log(cedula);
-    console.log(id_partido);
-    console.log(marcador1);
-    console.log(marcador2);
-    console.log(monto);
+      .catch(console.log('La consulta realizada ha sido fallida, intente de nuevo. '))
+   // console.log(cedula);
+   // console.log(id_partido);
+   // console.log(marcador1);
+   // console.log(marcador2);
+   // console.log(monto);
     query = 'INSERT INTO apuestas(`id_usuario`, `id_partido`, `goles_local`, `goles_visitante`, `monto`, `fecha_apuesta`, `jugado`, `pagado`) VALUES ('+id_usuario+','+id_partido+','+marcador1+','+marcador2+','+monto+',now(),0,0)';
     console.log(query);
     resultado = await consultadb(query)
@@ -434,9 +438,8 @@ ClienteARI.connect('http://localhost:8088', 'asterisk', 'asterisk', function (er
           text = 'Apuesta registrada Exitosamente. ';
       })
       .catch(text = 'La apuesta no fue registrada, intentelo de nuevo. ')
-    console.log(text);
     query = '';
-    text=text+'Gracias por preferirnos, esperamos que disfrutes de este mundial. Hasta pronto.';
+    text=text+'Gracias por preferirnos, esperamos que disfrutes de este mundial. Hasta pronto. ';
     console.log(text);
     await generarAudio(text);
     await convertirAudio();
@@ -453,6 +456,6 @@ ClienteARI.connect('http://localhost:8088', 'asterisk', 'asterisk', function (er
     }, 2000);
   }
 
-  ari.start('mundialFutbol2');
+  ari.start('mundialFutbol3');
 
 });
